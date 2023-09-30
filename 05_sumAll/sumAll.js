d2cbb298ea53b6d27624c426d9abb9881ba59e8e
PedroSicilia <32110851+pedrosicilia@users.noreply.github.com>
2023-09-30 06:09:36 -0400

    Ex 5 & 6 Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+
+    let num = 0;
+
+    if(!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0){
+        return "ERROR";
+    }
+    if(a < b)
+        for(let i = a; i <= b;i++)
+            num+= i;    
+    else if(b < a) 
+        for(let i = b; i <= a;i++)
+            num+= i;
+  
+    
+return num;
 
 };
 
